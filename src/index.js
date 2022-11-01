ef8b03874a557829d1733b533f12beec6cf22da9
Keelen Fisher <keelencarrera@gmail.com>
2022-11-01 11:35:49 -0500

    adding context to settings

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import SettingsProvider from './Context/Settings/Settings.js';
 import App from './App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <SettingsProvider>
+      <App />
+    </SettingsProvider>
   </React.StrictMode>
 );
