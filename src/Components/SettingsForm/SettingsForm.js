0fd0b8b2c7574aefb4b71af44273c3ae52234d42
Keelen Fisher <keelencarrera@gmail.com>
2022-11-02 13:25:13 -0500

    refactored and Auth added

--- src/Components/SettingsForm/SettingsForm.js
+++ src/Components/SettingsForm/SettingsForm.js
@@ -1,7 +1,8 @@
 import { useContext, useState } from 'react';
 import { SettingsContext } from '../../Context/Settings/Settings';
-import { Button, Card, Text, TextInput, createStyles, Grid, Switch } from '@mantine/core';
-
+import { Button, Card, Text, TextInput, createStyles, Grid, Switch, NumberInput } from '@mantine/core';
+import { When } from 'react-if';
+import { IconSettings } from '@tabler/icons';
 
 const useStyles = createStyles((theme) => ({
 
@@ -44,37 +45,48 @@ const FormSet = () => {
   // similar structure to todo jsx:
   return (
     <>
-      <h1 className={classes.h1}>Manage Settings</h1>
+      <h1 className={classes.h1}><IconSettings /> Manage Settings</h1>
       <Grid style={{ width: '80%', margin: 'auto' }}>
-        <Card>
-          <Text className={classes.settingCss}>Update Settings</Text>
-
-          <Switch
-            onChange={(e) => setShowCompleted(e.currentTarget.checked)}
-            checked={showCompleted}
-            label="Show Completed ToDos"
-            md="sm"
-          />
-
-          {/* Number Bar? How to make that. */}
-
-          <TextInput
-            mb="sm"
-            onChange={(e) => setSort(e.target.value)}
-            placeholder={sort}
-            label="Sort Keyword"
-          />
-          <Button onClick={handleClick}>Show New Settings</Button>
-        </Card>
-
-        <Card>
-          <Card.Section>
+        <Grid.Col xs={12} sm={6}>
+          <Card>
             <Text className={classes.settingCss}>Update Settings</Text>
-          </Card.Section>
-          {/* <Text m="sm">{showCompleted ? 'Show' : 'Hide'} Completed ToDos</Text>
+
+            <Switch
+              onChange={(e) => setShowCompleted(e.currentTarget.checked)}
+              checked={showCompleted}
+              label="Show Completed ToDos"
+              md="sm"
+            />
+
+            <NumberInput
+              mb="sm"
+              onChange={(value) => setPageItems(value)}
+              placeholder={pageItems}
+              label="Items Per page"
+            />
+
+            <TextInput
+              mb="sm"
+              onChange={(e) => setSort(e.target.value)}
+              placeholder={sort}
+              label="Sort Keyword"
+            />
+            <Button onClick={handleClick}>Show New Settings</Button>
+          </Card>
+
+        </Grid.Col>
+        <Grid.Col xs={12} sm={6}>
+          <When condition={show}>
+            <Card>
+              <Card.Section withBorder p="xs">
+                <Text className={classes.settingCss}>Update Settings</Text>
+              </Card.Section>
+              <Text m="sm">{showCompleted ? 'Show' : 'Hide'} Completed ToDos</Text>
             <Text m="sm">Items Per page:  {pageItems}</Text>
-            <Text m="sm">Sort Keyword: {sort}</Text> */}
-        </Card>
+            <Text m="sm">Sort Keyword: {sort}</Text>
+            </Card>
+          </When>
+        </Grid.Col>
       </Grid>
     </>
   )
